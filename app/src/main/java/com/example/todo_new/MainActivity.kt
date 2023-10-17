84fd8cff783e7337b7ca7db306f9f59bc6db671c
Lakshan Dissanayake <supunlakshan.xfinity@gmail.com>
2023-10-17 20:29:43 +0530

    fixed errors

--- app/src/main/java/com/example/todo_new/MainActivity.kt
+++ app/src/main/java/com/example/todo_new/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.example.todo_new
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.example.todo_new.databinding.ActivityMainBinding
+import com.example.todo_new.databinding.TodoBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val todoItems = mutableListOf<Todo>()
+    private lateinit var mAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.vrTodoItems.apply {
+            mAdapter = object : TodoAdapter(todoItems) {
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+                    return TodoViewHolder(TodoBinding.inflate(layoutInflater, parent, false))
+                }
+            }
+            adapter = mAdapter
+        }
+
+        binding.btnAddTodo.setOnClickListener {
+            binding.etTodoTitle.text.toString().let {
+                if (it.isNotEmpty()) {
+                    todoItems.add(0, Todo(it, false))
+                    mAdapter.notifyItemInserted(0)
+                    binding.etTodoTitle.text!!.clear()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/todo_new/TodoAdapter.kt
+++ app/src/main/java/com/example/todo_new/TodoAdapter.kt
@@ -1,16 +1,19 @@
 package com.example.todo_new
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todo_new.databinding.TodoBinding
 
 abstract class TodoAdapter(private val todos: MutableList<Todo>) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
-    class TodoViewHolder(itemView: TodoBinding) : RecyclerView.ViewHolder(itemView.root)
+    class TodoViewHolder(private val view: TodoBinding) : RecyclerView.ViewHolder(view.root) {
+        fun bind(item: Todo) {
+            view.tvToDoTit.text = item.title
+            view.cbDone.isChecked = item.isChecked
+        }
+    }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
         val curTodo = todos[position]
+        holder.bind(curTodo)
     }
 
     override fun getItemCount(): Int {
